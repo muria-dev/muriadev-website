7ddc6b5d4aaa0db4006458819b498c2cf7d06421
axzid <storyciks@gmail.com>
2022-11-15 13:32:19 +0700

    delete pembuat

--- components/About.jsx
+++ components/About.jsx
@@ -8,12 +8,12 @@ export default function About() {
           <Title title="Tentang Muria Dev" />
           <p className="text-slate-200 md:text-3xl text-2xl mt-8 md:leading-10 md:w-[80%] md:text-center text-left">
             Muria Dev adalah komunitas mahasiswa Universitas Muria Kudus
-            berbasis IT. Dibentuk pada tahun 2022 oleh zull dan terus berkembang
-            sampai sekarang. Muria Dev berfokus untuk menghasilkan karya di
-            bidang rekayasa perangkat lunak dan perangkat keras. Kami bekerja
-            sama secara individu maupun tim untuk menciptakan hasil yang
-            terbaik. Muria Dev dapat memberikan dampak dan pengalaman yang
-            positif bagi peminatnya.
+            berbasis IT. Dibentuk pada tahun 2022 dan terus berkembang sampai
+            sekarang. Muria Dev berfokus untuk menghasilkan karya di bidang
+            rekayasa perangkat lunak dan perangkat keras. Kami bekerja sama
+            secara individu maupun tim untuk menciptakan hasil yang terbaik.
+            Muria Dev dapat memberikan dampak dan pengalaman yang positif bagi
+            peminatnya.
           </p>
         </div>
       </div>
